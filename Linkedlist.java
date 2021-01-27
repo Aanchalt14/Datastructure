ff58a9f7009e04649c24093c130dd348dee747f1
Aanchalt14 <aanchalt71@gmail.com>
2021-01-27 15:16:01 +0530

    Add : Linkedlist program

---
+++ Linkedlist.java
@@ -0,0 +1,25 @@
+public class MyNode<K> {
+    private  MyNode next;
+    private K key;
+
+    public MyNode(K key) {
+        this.key = key;
+        this.next = null;
+    }
+
+    public MyNode getNext() {
+        return next;
+    }
+
+    public void setNext(MyNode next) {
+        this.next = next;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+}
