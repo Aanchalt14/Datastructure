e9efe4ab2a55d6cdce5b865ab506cb788a5663cf
Aanchalt14 <66659745+Aanchalt14@users.noreply.github.com>
2021-01-27 15:40:02 +0530

    Add files via upload

---
+++ MyNodeTest.java
@@ -0,0 +1,16 @@
+mport org.junit.Assert;
+import org.junit.Test;
+
+public class MyNodeTest {
+    @Test
+    public void given3Number_WhenLinkedShouldPassed_LinkedListTest() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> mythirdNode = new MyNode<>(70);
+        myFirstNode.setNext(mySecondNode);
+        mySecondNode.setNext(mythirdNode);
+        boolean result = myFirstNode.getNext().equals(mySecondNode) &&
+                myFirstNode.getNext().getNext().equals(mythirdNode);
+        Assert.assertTrue(result);
+    }
+}
